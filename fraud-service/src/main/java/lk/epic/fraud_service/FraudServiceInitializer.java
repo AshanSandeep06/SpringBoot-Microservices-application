86f852365f2e79c16e9765f92d0635da6e465625
AshanSandeep06 <ashansandeep06@gmail.com>
2023-08-08 21:56:57 +0530

    Content Updated

--- fraud-service/src/main/java/lk/epic/fraud_service/FraudServiceInitializer.java
+++ fraud-service/src/main/java/lk/epic/fraud_service/FraudServiceInitializer.java
@@ -4,8 +4,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class CustomerServiceInitializer {
+public class FraudServiceInitializer {
     public static void main(String[] args) {
-        SpringApplication.run(CustomerServiceInitializer.class, args);
+        SpringApplication.run(FraudServiceInitializer.class, args);
     }
 }
