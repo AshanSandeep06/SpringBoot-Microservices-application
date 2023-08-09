c0d755540e1bbc5b59ee9d3db29c1ba58c8a1fff
AshanSandeep06 <ashansandeep06@gmail.com>
2023-08-09 17:07:17 +0530

    Discovery Server and Discovery clients was refactored

--- customer-service/src/main/java/lk/epic/customer_service/entity/Customer.java
+++ customer-service/src/main/java/lk/epic/customer_service/entity/Customer.java
@@ -2,7 +2,8 @@ package lk.epic.customer_service.entity;
 
 import lombok.*;
 
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Id;
 
 @Data
 @Builder
--- fraud-service/src/main/java/lk/epic/fraud_service/FraudServiceApplication.java
+++ fraud-service/src/main/java/lk/epic/fraud_service/FraudServiceApplication.java
@@ -2,10 +2,12 @@ package lk.epic.fraud_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
-public class FraudServiceInitializer {
+@EnableEurekaClient
+public class FraudServiceApplication {
     public static void main(String[] args) {
-        SpringApplication.run(FraudServiceInitializer.class, args);
+        SpringApplication.run(FraudServiceApplication.class);
     }
 }
